3bfc1d9aee78f18e2e4e4a3a46c69217073b8ae9
harrissondutra <harrissondutra@gmail.com>
2024-02-25 20:23:52 -0300

    Criação do Layout da tela de Gerenciamento e finalização da tela de cadastro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,9 @@ android {
     namespace = "com.harrisson.cardapioonline"
     compileSdk = 34
 
+
+
+
     defaultConfig {
         applicationId = "com.harrisson.cardapioonline"
         minSdk = 24
@@ -37,7 +40,9 @@ android {
         viewBinding = true
         dataBinding = true
     }
+
 }
+
 
 dependencies {
 
@@ -49,5 +54,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.android.material:material:1.12.0-alpha03")
+
 }
---
+++ app/src/main/java/com/harrisson/cardapioonline/ui/activity/ManagerActivity.kt
@@ -0,0 +1,12 @@
+package com.harrisson.cardapioonline.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.harrisson.cardapioonline.R
+
+class ManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manager)
+    }
+}
